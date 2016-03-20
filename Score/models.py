82803c407d14d7ef520d106801a0bafa3c1ac553
Alexandre Cloquet <Alexandre.cloquet@gmail.com>
2016-03-20 18:18:13 +0100

    Add game + Card on game + migrate

--- Score/models.py
+++ Score/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Card(models.Model):
+    game = models.ForeignKey(Game)
+    name_card = models.CharField(max_length=256)
+    image = models.ImageField()
